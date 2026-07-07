f0740c1bc9b1f8579a79a7d7be90db64083478fa
Sakshath P Rao <sakshathprao1@gmail.com>
2026-07-07 21:11:04 +0530

    The server now keeps the connection alive

--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,7 @@ pub fn main(init: std.process.Init) !void {
     var stdout_inst = std.Io.File.stdout().writer(init.io, &stdout_buffer);
     var stdout_printer = &stdout_inst.interface;
 
-    try stdout_printer.print("A simple TCP server written in Zig {d}.{d}.{d}\n", .{ builtins.zig_version.major, builtins.zig_version.minor, builtins.zig_version.patch });
+    try stdout_printer.print("A simple TCP server written in Zig {d}.{d}.{d}\n\n", .{ builtins.zig_version.major, builtins.zig_version.minor, builtins.zig_version.patch });
     try stdout_inst.flush();
 
     try server(init.io);
@@ -15,26 +15,31 @@ pub fn main(init: std.process.Init) !void {
 
 fn handleAcceptedConnections(server_stream: std.Io.net.Stream, io: std.Io) !void {
     defer server_stream.close(io);
-    var server_read_buffer: [4000]u8 = undefined;
-    var server_client_response_buffer: [1][]u8 = .{server_read_buffer[0..]};
-    var server_write_buffer: [4000]u8 = undefined;
 
-    var stream_reader_inst = server_stream.reader(io, &server_read_buffer);
-    const server_reader = &stream_reader_inst.interface;
-    const client_input_size = try server_reader.readVec(&server_client_response_buffer);
+    while (true) {
+        var server_read_buffer: [4000]u8 = undefined;
+        var server_client_response_buffer: [1][]u8 = .{server_read_buffer[0..]};
+        var server_write_buffer: [4000]u8 = undefined;
 
-    var stream_writer_inst = server_stream.writer(io, &server_write_buffer);
-    const server_writer = &stream_writer_inst.interface;
+        var stream_reader_inst = server_stream.reader(io, &server_read_buffer);
+        const server_reader = &stream_reader_inst.interface;
+        const client_input_size = server_reader.readVec(&server_client_response_buffer) catch {
+            break;
+        };
 
-    if (client_input_size > 0) {
-        try server_writer.print("{s}", .{server_read_buffer[0..client_input_size]});
-        try server_writer.flush();
+        var stream_writer_inst = server_stream.writer(io, &server_write_buffer);
+        const server_writer = &stream_writer_inst.interface;
+
+        if (client_input_size > 0) {
+            try server_writer.print("{s}", .{server_read_buffer[0..client_input_size]});
+            try server_writer.flush();
+        }
     }
 }
 
 fn server(io: std.Io) !void {
-    const server_port: u14 = 8070;
     const server_address = "127.0.0.1";
+    const server_port = 8080;
 
     const IpAddress = std.Io.net.IpAddress;
     const server_address_parsed = try IpAddress.parse(server_address, server_port);
