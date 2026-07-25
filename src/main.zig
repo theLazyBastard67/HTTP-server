055347b2d38706145bb9bab94710749d589ce37a
Sakshath P Rao <sakshathprao1@gmail.com>
2026-07-25 15:56:30 +0530

    A semi-functional parser.

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const builtins = @import("builtin");
 
 pub fn main(init: std.process.Init) !void {
-    var stdout_buffer: [4000]u8 = undefined;
-    var stdin_buffer: [4000]u8 = undefined;
+    var stdout_buffer: [4096]u8 = undefined;
+    var stdin_buffer: [4096]u8 = undefined;
 
     var stdout_inst = std.Io.File.stdout().writer(init.io, &stdout_buffer);
     var stdout_printer = &stdout_inst.interface;
@@ -23,36 +23,70 @@ pub fn main(init: std.process.Init) !void {
     }
 }
 
-const httpRequestParsedStruct = struct { method: []const u8 = " ", filePath: []const u8 = " ", httpVersion: []const u8 = " ", host: []const u8 = " ", userAgent: []const u8 = " ", accept: []const u8 = " ", connection: []const u8 = " " };
+const httpRequestParsedStruct = struct { method: []const u8 = "", filePath: []const u8 = "", httpVersion: []const u8 = "", host: []const u8 = "", userAgent: []const u8 = "", accept: []const u8 = "", connection: []const u8 = "" };
 
 pub fn parseHttpRequests(inputBuffer: []u8) !httpRequestParsedStruct {
     const trim = std.mem.trim;
 
-    var tokenizer = std.mem.tokenizeAny(u8, inputBuffer, " \r\n");
-    const parseResult: httpRequestParsedStruct = .{ .method = tokenizer.next() orelse return error.InvalidRequest, .filePath = tokenizer.next() orelse return error.InvalidRequest, .httpVersion = tokenizer.next() orelse return error.InvalidRequest, .host = trim(u8, tokenizer.next() orelse return error.InvalidRequest, "Host:") };
+    var colon: usize = undefined;
+    var fieldName: []const u8 = undefined;
+    var fieldValue: []const u8 = undefined;
+    var parseResult: httpRequestParsedStruct = .{};
+
+    var tokenizer = std.mem.tokenizeAny(u8, inputBuffer, "\r\n");
+
+    if (tokenizer.next()) |request_line| {
+        var tokenizeRequestLine = std.mem.tokenizeScalar(u8, request_line, ' ');
+
+        parseResult.method = tokenizeRequestLine.next() orelse return error.InvalidRequest;
+        parseResult.filePath = tokenizeRequestLine.next() orelse return error.InvalidRequest;
+        parseResult.httpVersion = tokenizeRequestLine.next() orelse return error.InvalidRequest;
+    } else return error.InvalidRequest;
+
+    while (tokenizer.next()) |next_header_token| {
+        const trimmedTokenValue = trim(u8, next_header_token, " ");
+
+        colon = std.mem.find(u8, trimmedTokenValue, ":") orelse return error.InvalidRequest;
+
+        fieldName = trim(u8, trimmedTokenValue[0..colon], " ");
+        fieldValue = trim(u8, trimmedTokenValue[colon + 1 .. trimmedTokenValue.len], " ");
+
+        if (std.mem.eql(u8, fieldName, "Host")) {
+            parseResult.host = fieldValue;
+        }
+    }
     return parseResult;
 }
 
-fn handleAcceptedConnections(server_stream: std.Io.net.Stream, io: std.Io) !void {
+fn handleAcceptedConnections(server_stream: std.Io.net.Stream, io: std.Io, stdout_writer_interface: *std.Io.Writer) !void {
     defer server_stream.close(io);
 
-    var server_read_buffer: [4000]u8 = undefined;
-    var server_client_response_buffer: [1][]u8 = .{server_read_buffer[0..]};
-    var server_write_buffer: [4000]u8 = undefined;
+    var server_read_buffer: [4096]u8 = undefined;
+    var server_write_buffer: [4096]u8 = undefined;
 
     var stream_writer_inst = server_stream.writer(io, &server_write_buffer);
     const server_writer = &stream_writer_inst.interface;
 
     var stream_reader_inst = server_stream.reader(io, &server_read_buffer);
     const server_reader = &stream_reader_inst.interface;
+    var client_input_size: usize = 0;
 
     while (true) {
-        const client_input_size = server_reader.readVec(&server_client_response_buffer) catch {
+        var server_client_response_buffer: [1][]u8 = .{server_read_buffer[client_input_size..]};
+        client_input_size += server_reader.readVec(&server_client_response_buffer) catch {
             break;
         };
 
         if (client_input_size > 0) {
-            std.debug.print("\n{any}\n", .{try parseHttpRequests(server_read_buffer[0..client_input_size])});
+            const parseResult = parseHttpRequests(server_read_buffer[0..client_input_size]) catch |err| {
+                try stdout_writer_interface.print("\n{any}\n", .{err});
+                continue;
+            };
+
+            std.debug.print("{s} \n", .{parseResult.method});
+            std.debug.print("{s} \n", .{parseResult.filePath});
+            std.debug.print("{s} \n", .{parseResult.httpVersion});
+            std.debug.print("{s} \n", .{parseResult.host});
             try server_writer.print("{s}", .{server_read_buffer[0..client_input_size]});
             try server_writer.flush();
         }
@@ -73,6 +107,7 @@ fn server(io: std.Io, stdout_writer_interface: *std.Io.Writer, server_port: u16)
 
     while (true) {
         const server_stream = try server_inst.accept(io);
-        _ = try std.Thread.spawn(.{}, handleAcceptedConnections, .{ server_stream, io });
+        const thread = try std.Thread.spawn(.{}, handleAcceptedConnections, .{ server_stream, io, stdout_writer_interface });
+        thread.detach();
     }
 }
